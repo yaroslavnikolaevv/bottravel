3a7b451166c76e5b001c205b306abd946353dc04
yaroslavnikolaevv <51954484+yaroslavnikolaevv@users.noreply.github.com>
2019-11-24 15:38:33 +0300

    Update capital.py

--- capital.py
+++ capital.py
@@ -1,10 +1,10 @@
 class Search_capital:
 	def __init__(self,search):
-        	self.searchIn=search
-	def search(self):	
-		a=self.searchIn
+        	self.search=search.lower()
+	def poisk(self):	
+		a=self.search
 		file=open('base_date_city.txt','r',encoding='UTF-8')
 		for line in file:
 			if a in line:
-			    capital=line[0:line.index(':')]
+			    capital=line[:line.index(':')]
 			    return(capital)
