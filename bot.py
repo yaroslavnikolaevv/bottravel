6accc119a56f12f08c00791d78e517fc25812909
RustKnight7 <57863102+RustKnight7@users.noreply.github.com>
2019-11-25 20:08:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,8 @@ for i in taxicities:
     key = i[:i.index(';')]
     taxidict[key] = i[i.index(';') + 2:]
 #Блок стикеров
-commandlist = {'/wikipedia' : 'wikipedia_message(message)', '/wiki' : 'wikipedia_message(message)', '/start': 'start_message(message)', '/help' : 'help_message(message)', '/tickets' : 'tickets_message(message)', '/route' : 'tickets_message(message)', '/weather' : 'weather_message(message)', '/music' : 'music_message(message)', '/developers' : 'developers_message(message)', '/taxi' : 'taxi_message(message)', '/video' : 'video_message(message)'}
-commandlist_ru = {'википедия' : 'wikipedia_message(message)', 'вики' : 'wikipedia_message(message)', 'старт': 'start_message(message)', 'помощь' : 'help_message(message)','билеты' : 'tickets_message(message)', 'маршрут' : 'tickets_message(message)', 'погода' : 'weather_message(message)', 'музыка' : 'music_message(message)', 'разработчики' : 'developers_message(message)', 'такси' : 'taxi_message(message)', 'видео' : 'video_message(message)'}
+commandlist = {'/advice' : 'advice_message(message)', '/wikipedia' : 'wikipedia_message(message)', '/wiki' : 'wikipedia_message(message)', '/start': 'start_message(message)', '/help' : 'help_message(message)', '/tickets' : 'tickets_message(message)', '/route' : 'tickets_message(message)', '/weather' : 'weather_message(message)', '/music' : 'music_message(message)', '/developers' : 'developers_message(message)', '/taxi' : 'taxi_message(message)', '/video' : 'video_message(message)'}
+commandlist_ru = {'советы' : 'advice_message(message)', 'википедия' : 'wikipedia_message(message)', 'вики' : 'wikipedia_message(message)', 'старт': 'start_message(message)', 'помощь' : 'help_message(message)','билеты' : 'tickets_message(message)', 'маршрут' : 'tickets_message(message)', 'погода' : 'weather_message(message)', 'музыка' : 'music_message(message)', 'разработчики' : 'developers_message(message)', 'такси' : 'taxi_message(message)', 'видео' : 'video_message(message)'}
 lovestickerpack = ['CAADAgAD2QADVp29CtGSZtLSYweoFgQ', 'CAADAgAD0gADVp29Cg4FcjZ1gzWKFgQ', 'CAADAgAD0wADVp29CvUyj5fVEvk9FgQ', 'CAADAgAD2AADVp29CokJ3b9L8RQnFgQ', 'CAADAgAD3gADVp29CqXvdzhVgxXEFgQ', 'CAADAgADFQADwDZPE81WpjthnmTnFgQ', 'CAADAgADBQADwDZPE_lqX5qCa011FgQ', 'CAADAgADDQADwDZPE6T54fTUeI1TFgQ', 'CAADAgADHQADwDZPE17YptxBPd5IFgQ', 'CAADAgAD4QcAAnlc4gndRsN-Tyzk1xYE', 'CAADAgAD3wcAAnlc4gmeYgfVO_CEsxYE', 'CAADAgAD4AcAAnlc4gmXqeueTbWXlRYE', ]
 questionstickerpack = ['CAADAgAD4wADVp29Cg_4Isytpgs3FgQ', 'CAADAgADEgADwDZPEzO8ngEulQc3FgQ', 'CAADAgADEAADwDZPE-qBiinxHwLoFgQ', 'CAADAgADIAADwDZPE_QPK7o-X_TPFgQ', 'CAADAgAD2wcAAnlc4gkSqCLudDgLbhYE', 'CAADAgADzwcAAnlc4gnrZCnufdBTahYE', 'CAADAgAD2QcAAnlc4gn3Ww8qzk3S3BYE', 'CAADAgAD0gcAAnlc4gmLqZ82yF4OlxYE']
 angrystickerpack = ['CAADAgAD3AADVp29Cpy9Gm5Tg192FgQ', 'CAADAgAD2wADVp29Clxn-p9taVttFgQ', 'CAADAgADywADVp29CllGpcs9gzQoFgQ']
@@ -40,7 +40,8 @@ nongratlist = ['арина', 'ариша', 'алия']
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('такси', 'билеты', 'погода')
 keyboard1.row('музыка', 'видео', 'вики')
-keyboard1.row('старт', 'помощь', 'разработчики')
+keyboard1.row('советы', 'помощь')
+keyboard1.row('старт', 'разработчики')
 #Блок погоды
 owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language = 'ru, en')
 bot = telebot.TeleBot(token)
@@ -53,6 +54,14 @@ video_search_list = []
 videos_for_dict={}
 res = ''
 status = ''
+#Блок для советов
+@bot.message_handler(commands=['advice'])
+def advice_message(message):
+	bot.send_message(message.chat.id, 'Документы:\nпаспорт: внутренний или загран; документы для ребенка: 1) паспорт, 2) свидетельство о рождении, 3) согласие на выезд из России, если ребенок едет за границу без родителей; наличные деньги; билеты на самолет, поезд, автобус; брони отелей; водительские права; копия паспорта; страховой полис путешественника')
+	bot.send_message(message.chat.id, 'Техника и гаджеты в дорогу: \ncмартфон и зарядка; внешний жесткий диск; дорожный утюг; маленький электрический чайник; наушники; ноутбук и зарядка; переходник для розеток; плеер; тройник, удлинитель или сетевой фильтр; фен; фотоаппарат, зарядка, карты памяти, сумка для камеры; штатив, монопод, палка для селфи; электронная книга')
+	bot.send_message(message.chat.id, 'Бытовые мелочи и комфорт в поездке: \nсумочка или городской рюкзак для прогулок; блокнот и ручка; вилка, ложка, тарелка, чашка; зонт; карманное зеркало; карта; книга, путеводитель, журнал; маска для сна, беруши, надувная подушка; обычные пакеты; полотенце; разговорник; солнечные очки; туалетная бумага; швейный набор; швейцарский армейский нож')
+	bot.send_message(message.chat.id, 'Гигиена и косметика в поездку: \nбритва; дезодорант; зубная паста и щетка; расческа; ватные палочки, ватные диски; Влажные салфетки, бумажные платочки; гигиеническая помада, бальзам для губ; гигиенические прокладки, тампоны; дезинфицирующий гель для рук; зубная нить, зубочистки; крем от солнца; кремы для лица и тела; ножницы и пилочка для ногтей; очки или контактные линзы с контейнером и раствором; парфюм; пена для и после бритья; помада, тушь для ресниц и другая декоративная косметика, средство для снятия макияжа; презервативы; репеллент от комаров; средство для укладки волос; фумигатор; шампунь, кондиционер для волос, мыло, гель для душа, мочалка')
+
 #Блок для Википедии
 @bot.message_handler(commands=['wiki', 'wikipedia'])
 def wikipedia_message(message):
